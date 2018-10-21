b7d21eb1406fdfb7034f45229b84fe245ba201b7
akankshapatel <90akankshapatel@gmail.com>
2018-10-21 08:51:55 +0530

    Updated solution for lc496_next_grreater_element.cpp

--- lc496_next_greater_element.cpp
+++ lc496_next_greater_element.cpp
@@ -1,4 +1,4 @@
-ass Solution {
+class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& findNums, vector<int>& nums) {
         vector<int> outputNums;
@@ -11,7 +11,7 @@ public:
                 numsStack.push(nums.at(j));
             
             int ngn = -1; // ngn = next greater number for findNums.at(i)
-            while(numsStack.top()){
+            while(numsStack.size()){
                 if(numsStack.top() > findNums.at(i)) {
                     ngn = numsStack.top();
                 }
@@ -26,3 +26,62 @@ public:
         return outputNums;
     }
 };
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+vector<int> stringToIntegerVector(string input) {
+    vector<int> output;
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    stringstream ss;
+    ss.str(input);
+    string item;
+    char delim = ',';
+    while (getline(ss, item, delim)) {
+        output.push_back(stoi(item));
+    }
+    return output;
+}
+
+string integerVectorToString(vector<int> list, int length = -1) {
+    if (length == -1) {
+        length = list.size();
+    }
+
+    if (length == 0) {
+        return "[]";
+    }
+
+    string result;
+    for(int index = 0; index < length; index++) {
+        int number = list[index];
+        result += to_string(number) + ", ";
+    }
+    return "[" + result.substr(0, result.length() - 2) + "]";
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        vector<int> nums1 = stringToIntegerVector(line);
+        getline(cin, line);
+        vector<int> nums2 = stringToIntegerVector(line);
+        
+        vector<int> ret = Solution().nextGreaterElement(nums1, nums2);
+
+        string out = integerVectorToString(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
