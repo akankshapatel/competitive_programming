5320eef9a52abfc115834107b51b68b5b94e8e2b
akankshapatel <90akankshapatel@gmail.com>
2018-11-01 12:47:14 +0530

    solved again

--- lc237_delete_node_in_a_linked_list.cpp
+++ lc237_delete_node_in_a_linked_list.cpp
@@ -1,24 +1,17 @@
-// Edited
-
-
-/**
- ** Definition for singly-linked list.
- ** struct ListNode {
- **     int val;
- **     ListNode *next;
- **     ListNode(int x) : val(x), next(NULL) {}
- ** };
- **/
+/***
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
 class Solution {
-    public:
-        void deleteNode(ListNode* node) {
-            ListNode* n = node;
-            while(n->next->next != NULL){
-                n->val = n->next->val;
-                n = n->next;
-            }
-            n->val = n->next->val;
-            n->next = NULL;
-
-        }
+public:
+    void deleteNode(ListNode* node) {        
+        ListNode *n = node;
+        n->val = n->next->val;
+        n->next = n->next->next;
+        node = NULL;
+    }
 };
